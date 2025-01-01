334699a1a127705256cee262a3373c08144de190
demian-astudillo <dem.astudillo@gmail.com>
2025-01-01 17:14:08 -0300

    inicio desafio

---
+++ src/desafio.java
@@ -0,0 +1,15 @@
+public class desafio {
+
+    public static void main(String[] args) {
+
+        String nombre = "cato";
+        String cuenta = "paypal";
+        double saldo = 1500.0;
+
+        System.out.println("*****");
+        System.out.println("\nNombre del Cliente: " + nombre);
+        System.out.println("Tipo de Cuenta: " + cuenta);
+        System.out.println("Saldo Disponible: " + saldo);
+
+    }
+}
